ff1fd01216758ad5cbb4ae8954e1cb021908cb91
Thomas Johnson <thomasij813@gmail.com>
2020-01-17 15:08:20 -0800

    Add test for findOne in household service

--- src/household/household.service.spec.ts
+++ src/household/household.service.spec.ts
@@ -1,10 +1,21 @@
 import { HouseholdService } from './household.service';
+import { CreateHouseholdDto } from './dto/create-household.dto';
 
 describe('HouseholdService', () => {
   let service: HouseholdService;
+  let createHouseholdDto: CreateHouseholdDto;
 
   beforeEach(async () => {
     service = new HouseholdService();
+    createHouseholdDto = {
+      email: 'thomasij813@gmail.com',
+      first: 'Thomas',
+      last: 'Johnson',
+      street1: '106 14th Ave E',
+      city: 'Seattle',
+      state: 'WA',
+      zip: '98112',
+    };
   });
 
   it('should be defined', () => {
@@ -19,16 +30,6 @@ describe('HouseholdService', () => {
 
   describe('create', () => {
     it('Should create a household from a createHouseholdDto', () => {
-      const createHouseholdDto = {
-        email: 'thomasij813@gmail.com',
-        first: 'Thomas',
-        last: 'Johnson',
-        street1: '106 14th Ave E',
-        city: 'Seattle',
-        state: 'WA',
-        zip: '98112',
-      };
-
       const household = service.create(createHouseholdDto);
 
       expect(household).toHaveProperty('id');
@@ -43,21 +44,24 @@ describe('HouseholdService', () => {
 
   describe('delete', () => {
     it('Should delete a household by id', () => {
-      const createHouseholdDto = {
-        email: 'thomasij813@gmail.com',
-        first: 'Thomas',
-        last: 'Johnson',
-        street1: '106 14th Ave E',
-        city: 'Seattle',
-        state: 'WA',
-        zip: '98112',
-      };
-
       const household = service.create(createHouseholdDto);
 
       const { id } = household;
       service.delete(id);
       expect(service.findAll().length).toBe(0);
+    });
+  });
+
+  describe('findOne', () => {
+    it('Should find one household by id', () => {
+      const household = service.create(createHouseholdDto);
+      const { id } = household;
+
+      expect(service.findOne(id).id).toEqual(id);
+    });
+
+    it('Should throw an error if no houshold is found', () => {
+      expect(() => service.findOne('NOT FOUND')).toThrowError();
     })
-  })
+  });
 });
